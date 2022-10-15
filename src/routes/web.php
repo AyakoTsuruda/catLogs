f6c0e2ade10f63459be91ed1ce0029293f3ae61a
tsuruda-a <tsuruda-a@glug.co.jp>
2022-10-15 16:52:58 +0900

    aaa

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+/**
+ * 管理者メニュー
+ */
+require __DIR__ . '/administrator.php';
+
+/**
+ * メンバーメニュー
+ */
+require __DIR__ . '/member.php';
+
+/**
+ * ユーザーメニュー
+ */
+require __DIR__ . '/user.php';
